bdebc4f288c9cea204e6e28c886dfd98d5e1c7f3
Jonatan Santos <jonatansilvasantos@outlook.com>
2025-04-01 16:06:40 -0300

    adicionado metodo de login com conta google, adicionado arquivo env.example para quem acessar o projeto possa saber as variáveis necessárias

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,14 +1,47 @@
+import { useState } from "react";
 import { Container, Slogan, Title } from "./styles";
 import backgroundImg from "../../assets/background.png";
 import { Button } from "../../components/Button";
 
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import { WEB_CLIENT_ID } from "@env";
+import { Alert } from "react-native";
+
+GoogleSignin.configure({
+  scopes: ["email", "profile"],
+  webClientId: WEB_CLIENT_ID,
+});
+
 export function SignIn() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function handleGoogleSignIn() {
+    try {
+      setIsAuthenticating(true);
+
+      const response = await GoogleSignin.signIn();
+
+      const idToken = response?.data?.idToken;
+
+      if (idToken) {
+        console.log(idToken);
+      }
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Entrar", "Não foi possível conectar-se a sua conta Google.");
+      setIsAuthenticating(false);
+    }
+  }
   return (
     <Container source={backgroundImg}>
       <Title>Ignite Fleet</Title>
       <Slogan>Gestão de uso de veículos</Slogan>
 
-      <Button title="Entrar com Google" />
+      <Button
+        title="Entrar com Google"
+        isLoading={isAuthenticating}
+        onPress={handleGoogleSignIn}
+      />
     </Container>
   );
 }
